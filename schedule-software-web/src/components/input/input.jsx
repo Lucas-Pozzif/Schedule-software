3fb8b48c20b6a852ad9688785d25e6ae7bef80f8
Lucas <lucaspozzif20@gmail.com>
2023-02-05 23:32:51 -0300

    input component creation

--- schedule-software-web/src/components/input/input.jsx
+++ schedule-software-web/src/components/input/input.jsx
@@ -11,12 +11,10 @@ export function Input
     }) {
 
     return (
-        <label>
-            {label}
-            <input className="input" placeholder={placeholder} value={value} onChange={(value) => {
-                setValue(value.target.value)
-            }}></input>
-        </label>
+        <div className='input-div'>
+            <input className="input" placeholder={placeholder} value={value} onChange={(value) => setValue(value.target.value)}></input>
+
+        </div>
     )
 
 }
