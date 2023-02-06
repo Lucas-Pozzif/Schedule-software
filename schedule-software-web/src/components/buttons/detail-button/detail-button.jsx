58ad357255bf1666fcdbc23b7fa59d8facf1bc21
Lucas <lucaspozzif20@gmail.com>
2023-02-06 18:38:06 -0300

    detail buton minor change

--- schedule-software-web/src/components/buttons/detail-button/detail-button.jsx
+++ schedule-software-web/src/components/buttons/detail-button/detail-button.jsx
@@ -1,8 +1,9 @@
 import './detail-button.css'
 
-export function DetailButton(parameters) {
-    const text = parameters.text
-    const status = parameters.status
+export function DetailButton({
+    text,
+    status
+}) {
     return (
         <div className={`detail-button ${status ? 'secondary-color-background' : 'terciary-color-background'}`}>
             <p className={`detail-button-text ${status ? 'terciary-color' : 'secondary-color'}`}>{text}</p>
