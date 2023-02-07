0f89718036378ed0244c01b5c4436375bff544e9
Lucas <lucaspozzif20@gmail.com>
2023-02-07 16:11:34 -0300

    edit professional delete tab created

---
+++ schedule-software-web/src/pages/professional/edit-professional/delete/delete.jsx
@@ -0,0 +1,52 @@
+import { SmallButton } from "../../../../components/buttons/small-button/small-button";
+import { ItemButton } from "../../../../components/buttons/item-button/item-button";
+import { localLanguage } from "../../../../data/language-selector";
+
+import { useNavigate } from "react-router-dom";
+
+import './delete.css'
+
+export function EPDelete({
+    professional,
+    setProfessional,
+    setPage
+
+}) {
+    const navigate = useNavigate()
+
+    function deleteProfessional() {
+        setProfessional({})
+        setPage('deleted')
+        setTimeout(() => {
+            navigate('/')
+        }, 3000);
+
+    }
+
+    return (
+        <div className="edit-professional-delete">
+            <div className="epd-header">
+                <p className="epd-title">{localLanguage.editProfessional.delete.title}</p>
+                <div className="epd-subtitle-block">
+                    <p className="epd-subtitle">{localLanguage.editProfessional.delete.subtitle}</p>
+                    <div onClick={() => {
+                        deleteProfessional()
+                    }}>
+                        <SmallButton light={true} text={localLanguage.editProfessional.delete.buttonText}></SmallButton>
+                    </div>
+                </div>
+            </div>
+            <div className="epd-item-button" >
+                <ItemButton
+
+                    image={professional.photo}
+                    title={professional.name}
+                    subtitle={professional.occupations}
+
+                />
+
+            </div>
+
+        </div>
+    )
+}
