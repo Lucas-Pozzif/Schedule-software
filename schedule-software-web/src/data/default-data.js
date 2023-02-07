ee353ee3453671dcd6ba3598de4b4db6cb30f51a
Lucas <lucaspozzif20@gmail.com>
2023-02-07 16:12:22 -0300

    debug data changes

--- schedule-software-web/src/data/default-data.js
+++ schedule-software-web/src/data/default-data.js
@@ -1,3 +1,24 @@
 const defaultRange = 5;
 
-export { defaultRange }
+function generateTimeValues() {
+    const time = [];
+
+
+    for (let i = 0; i < 24; i++) {
+        for (let j = 0; j < 4; j++) {
+            var minutes = `${j * 15}`
+            if (minutes.length <= 1) {
+                minutes = `0${minutes}`
+            }
+
+            const timeValue = `${i}:${minutes}`
+            time.push(timeValue)
+        }
+    }
+    return time
+}
+
+const time = generateTimeValues()
+
+
+export { defaultRange, time as timeData }
