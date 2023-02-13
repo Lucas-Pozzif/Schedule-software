d7f24a0d06d84a655008040fe57c4ce9583c76b3
Lucas <lucaspozzif20@gmail.com>
2023-02-13 14:00:20 -0300

    firebase user account and login first tests

--- schedule-software-web/src/data/data management/database-management.js
+++ schedule-software-web/src/data/data management/database-management.js
@@ -3,6 +3,11 @@ import professionals from "../database/professionals.json";
 import configuration from "../database/configuration.json";
 import services from "../database/services.json";
 
+import { firebaseConfig } from "./firebase-config";
+
+import { initializeApp } from "firebase/app";
+import { GoogleAuthProvider } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 const configData = configuration;
 
 const langData = languages.ptBr;
@@ -10,4 +15,9 @@ const langData = languages.ptBr;
 const profData = professionals;
 const servData = services;
 
-export { profData, langData, configData,servData };
+export { profData, langData, configData, servData };
+
+const app = initializeApp(firebaseConfig)
+
+export const provider = new GoogleAuthProvider()
+export const db = getFirestore(app)
